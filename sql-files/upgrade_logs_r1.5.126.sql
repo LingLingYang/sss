0ff1dfb61e48f0f11680cbd20b0c423297026db7
Inkfish <Inkfish@99079514-12a4-4c38-a3c0-052ab5c826f2>
2015-03-20 07:04:30 +0000

    2015/03/20[SeAr_1.10.218]

--- sql-files/upgrade_logs_r1.5.126.sql
+++ sql-files/upgrade_logs_r1.5.126.sql
@@ -4,6 +4,7 @@
 CREATE TABLE `cashlog` (
   `id` int(11) NOT NULL auto_increment,
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `account_id` int(11) NOT NULL default '0',
   `char_id` int(11) NOT NULL default '0',
   `type` enum('C','K') NOT NULL default 'C',
   `amount` int(11) NOT NULL default '0',
