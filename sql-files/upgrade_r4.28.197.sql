1b4041a43cdd7e15f23161c180185eb95a8e6a85
lnkfish <lnkfish@99079514-12a4-4c38-a3c0-052ab5c826f2>
2016-07-07 07:21:35 +0000

    2016/7/7[SeAr_4.28.197]

---
+++ sql-files/upgrade_r4.28.197.sql
@@ -0,0 +1,34 @@
+UPDATE `auction` SET `value2_0` = 0;
+UPDATE `mail` SET `value2_0` = 0;
+UPDATE `cart_inventory` SET `value2_0` = 0;
+UPDATE `guild_storage` SET `value2_0` = 0;
+UPDATE `inventory` SET `value2_0` = 0;
+UPDATE `storage` SET `value2_0` = 0;
+
+UPDATE `auction` SET `value2_1` = 0;
+UPDATE `mail` SET `value2_1` = 0;
+UPDATE `cart_inventory` SET `value2_1` = 0;
+UPDATE `guild_storage` SET `value2_1` = 0;
+UPDATE `inventory` SET `value2_1` = 0;
+UPDATE `storage` SET `value2_1` = 0;
+
+UPDATE `auction` SET `value2_2` = 0;
+UPDATE `mail` SET `value2_2` = 0;
+UPDATE `cart_inventory` SET `value2_2` = 0;
+UPDATE `guild_storage` SET `value2_2` = 0;
+UPDATE `inventory` SET `value2_2` = 0;
+UPDATE `storage` SET `value2_2` = 0;
+
+UPDATE `auction` SET `value2_3` = 0;
+UPDATE `mail` SET `value2_3` = 0;
+UPDATE `cart_inventory` SET `value2_3` = 0;
+UPDATE `guild_storage` SET `value2_3` = 0;
+UPDATE `inventory` SET `value2_3` = 0;
+UPDATE `storage` SET `value2_3` = 0;
+
+UPDATE `auction` SET `value2_4` = 0;
+UPDATE `mail` SET `value2_4` = 0;
+UPDATE `cart_inventory` SET `value2_4` = 0;
+UPDATE `guild_storage` SET `value2_4` = 0;
+UPDATE `inventory` SET `value2_4` = 0;
+UPDATE `storage` SET `value2_4` = 0;
